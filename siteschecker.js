66045ebae0bcbfa07073737866b1ae165188df9d
pkwiecien <patryk.kwiecien@deviniti.com>
2022-12-27 19:05:20 +0100

    Add sending an email if the site is not available

--- siteschecker.js
+++ siteschecker.js
@@ -1,19 +1,55 @@
 import express from 'express';
 import axios from 'axios';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = 3000;
+dotenv.config();
+
+
+const emailMessage = (siteUrl) => {
+  const mailOptions = {
+    from: 'site-checker@kwiecien.dev',
+    to: 'kwiecienpatryk@gmail.com',
+    subject: `Unavailable: ${siteUrl}`,
+    text: `This site ${siteUrl} is unavailable`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Wiadomość została wysłana: ' + info.response);
+    }
+  });
+}
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.USERNAME,
+    pass: process.env.PASSWORD
+  }
+});
+
 
 const checkAvailability = async (url) => {
   try {
     const response = await axios.get(url);
+
+    emailMessage(url);
+
     return {
       url: url,
       status: 'available',
       date: new Date()
-
     };
   } catch (error) {
+    emailMessage(url);
+
     return {
       url: url,
       status: 'unavailable',
